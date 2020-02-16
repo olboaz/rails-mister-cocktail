5dd4335924b946d7ff04b65dd53db86e99a3aeba
patrice jaszczynski <ppastg92@free.fr>
2020-02-16 19:24:16 +0100

    add gif

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-require 'json'
-require 'open-uri'
+  require 'json'
+  require 'open-uri'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+  ingredients_serialized = open(url).read
+  ingredients = JSON.parse(ingredients_serialized)
 
-ingredients["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient["strIngredient1"])
-end
+  ingredients["drinks"].each do |ingredient|
+    Ingredient.create(name: ingredient["strIngredient1"])
+  end
 
 # Ingredient.create(name: "lemon")
 # Ingredient.create(name: "ice")
